6b5424c049ea55774234322d75ffde2f930ca0f2
TamirtheProgrammer <TamirGHCA@users.noreply.github.com>
2021-11-23 20:24:25 +0200

    first commit

---
+++ Javascript File.js
@@ -0,0 +1,19 @@
+
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Sentence changed.";
+}
+</script>
+
+<pre>
+  <code>
+    x = 5;
+    y = 6;
+    z = x + y;
+  </code>
+</pre>
+
+<script>document.getElementById("demo").innerHTML = "WELCOME NYUKKA!";</script>
+
+<script> document.write(PP small)</script>
